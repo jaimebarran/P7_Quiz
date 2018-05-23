d754d4449a39bbc22031c4365f6bd86970e89b5e
jaimebarran <j.barrancoh@alumnos.upm.es>
2018-05-23 21:28:27 +0200

    232128

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,294 @@
+var models = require("../models");
+var Sequelize = require('sequelize');
+
+var paginate = require('../helpers/paginate').paginate;
+
+// Autoload el quiz asociado a :quizId
+exports.load = function (req, res, next, quizId) {
+
+    models.Quiz.findById(quizId, {
+        include: [
+            {model: models.Tip, 
+              include: [
+                 {model: models.User, as:'Author'}
+                 ]
+            },
+            {model: models.User, as: 'Author'}
+        ]
+    })
+    .then(function (quiz) {
+        if (quiz) {
+            req.quiz = quiz;
+            next();
+        } else {
+            throw new Error('No existe ningún quiz con id=' + quizId);
+        }
+    })
+    .catch(function (error) {
+        next(error);
+    });
+};
+
+
+// MW que permite acciones solamente si al usuario logeado es admin o es el autor del quiz.
+exports.adminOrAuthorRequired = function(req, res, next){
+
+    var isAdmin  = req.session.user.isAdmin;
+    var isAuthor = req.quiz.AuthorId === req.session.user.id;
+
+    if (isAdmin || isAuthor) {
+        next();
+    } else {
+        console.log('Operación prohibida: El usuario logeado no es el autor del quiz, ni un administrador.');
+        res.send(403);
+    }
+};
+
+
+// GET /quizzes
+exports.index = function (req, res, next) {
+
+    var countOptions = {
+        where: {}
+    };
+
+    var title = "Preguntas";
+
+    // Busquedas:
+    var search = req.query.search || '';
+    if (search) {
+        var search_like = "%" + search.replace(/ +/g,"%") + "%";
+
+        countOptions.where.question = { $like: search_like };
+    }
+
+    // Si existe req.user, mostrar solo sus preguntas.
+    if (req.user) {
+        countOptions.where.AuthorId = req.user.id;
+        title = "Preguntas de " + req.user.username;
+    }
+
+    models.Quiz.count(countOptions)
+    .then(function (count) {
+
+        // Paginacion:
+
+        var items_per_page = 10;
+
+        // La pagina a mostrar viene en la query
+        var pageno = parseInt(req.query.pageno) || 1;
+
+        // Crear un string con el HTML que pinta la botonera de paginacion.
+        // Lo añado como una variable local de res para que lo pinte el layout de la aplicacion.
+        res.locals.paginate_control = paginate(count, items_per_page, pageno, req.url);
+
+        var findOptions = countOptions;
+
+        findOptions.offset = items_per_page * (pageno - 1);
+        findOptions.limit = items_per_page;
+        findOptions.include = [{model: models.User, as: 'Author'}];
+
+        return models.Quiz.findAll(findOptions);
+    })
+    .then(function (quizzes) {
+        res.render('quizzes/index.ejs', {
+            quizzes: quizzes,
+            search: search,
+            title: title
+        });
+    })
+    .catch(function (error) {
+        next(error);
+    });
+};
+
+
+// GET /quizzes/:quizId
+exports.show = function (req, res, next) {
+
+    res.render('quizzes/show', {quiz: req.quiz});
+};
+
+
+// GET /quizzes/new
+exports.new = function (req, res, next) {
+
+    var quiz = {question: "", answer: ""};
+
+    res.render('quizzes/new', {quiz: quiz});
+};
+
+
+// POST /quizzes/create
+exports.create = function (req, res, next) {
+
+    var authorId = req.session.user && req.session.user.id || 0;
+
+    var quiz = models.Quiz.build({
+        question: req.body.question,
+        answer: req.body.answer,
+        AuthorId: authorId
+    });
+
+    // guarda en DB los campos pregunta y respuesta de quiz
+    quiz.save({fields: ["question", "answer", "AuthorId"]})
+    .then(function (quiz) {
+        req.flash('success', 'Quiz creado con éxito.');
+        res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(Sequelize.ValidationError, function (error) {
+
+        req.flash('error', 'Errores en el formulario:');
+        for (var i in error.errors) {
+            req.flash('error', error.errors[i].value);
+        }
+
+        res.render('quizzes/new', {quiz: quiz});
+    })
+    .catch(function (error) {
+        req.flash('error', 'Error al crear un Quiz: ' + error.message);
+        next(error);
+    });
+};
+
+
+// GET /quizzes/:quizId/edit
+exports.edit = function (req, res, next) {
+
+    res.render('quizzes/edit', {quiz: req.quiz});
+};
+
+
+// PUT /quizzes/:quizId
+exports.update = function (req, res, next) {
+
+    req.quiz.question = req.body.question;
+    req.quiz.answer = req.body.answer;
+
+    req.quiz.save({fields: ["question", "answer"]})
+    .then(function (quiz) {
+        req.flash('success', 'Quiz editado con éxito.');
+        res.redirect('/quizzes/' + req.quiz.id);
+    })
+    .catch(Sequelize.ValidationError, function (error) {
+
+        req.flash('error', 'Errores en el formulario:');
+        for (var i in error.errors) {
+            req.flash('error', error.errors[i].value);
+        }
+
+        res.render('quizzes/edit', {quiz: req.quiz});
+    })
+    .catch(function (error) {
+        req.flash('error', 'Error al editar el Quiz: ' + error.message);
+        next(error);
+    });
+};
+
+
+// DELETE /quizzes/:quizId
+exports.destroy = function (req, res, next) {
+
+    req.quiz.destroy()
+    .then(function () {
+        req.flash('success', 'Quiz borrado con éxito.');
+        res.redirect('/goback');
+    })
+    .catch(function (error) {
+        req.flash('error', 'Error al editar el Quiz: ' + error.message);
+        next(error);
+    });
+};
+
+
+// GET /quizzes/:quizId/play
+exports.play = function (req, res, next) {
+
+    var answer = req.query.answer || '';
+
+    res.render('quizzes/play', {
+        quiz: req.quiz,
+        answer: answer
+    });
+};
+
+// GET /quizzes/:quizId/randomplay
+exports.randomplay = function (req, res, next) {
+	
+	if(!req.session.played){
+	    req.session.played=[];
+	    req.session.score=0;		
+		}
+	
+	var played = req.session.played;
+
+	models.Quiz.findAll()
+	.then(function(quizzes){
+		
+		console.log(played.length);
+		if(played.length >= quizzes.length){
+			res.render('quizzes/random_nomore', {
+                	score: req.session.score
+            });
+        }else{
+			
+		var j=Math.floor(quizzes.length*Math.random());
+		var quizzId=quizzes[j].id;
+		console.log(quizzId);
+		while (played.indexOf(quizzId) !== -1){
+			j=Math.floor(quizzes.length*Math.random());
+			quizzId=quizzes[j].id;}
+		}
+		return models.Quiz.findById(quizzId);
+	})
+	.then(function(quizz) {
+		console.log(quizz.id);
+		req.session.played.push(quizz.id);
+                res.render('quizzes/randomplay', {
+                	quiz: quizz,
+                	score: req.session.score,
+		});
+	})
+	.catch(function(error) {
+        req.flash('error', 'Error al cargar el Quiz: ' + error.message);
+        next(error);
+    });
+
+};
+
+
+
+
+// GET /quizzes/:quizId/check
+exports.check = function (req, res, next) {
+
+    var answer = req.query.answer || "";
+
+    var result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
+
+    res.render('quizzes/result', {
+        quiz: req.quiz,
+        result: result,
+        answer: answer
+    });
+};
+
+// GET /quizzes/randomcheck/:quizId
+exports.randomcheck = function (req, res, next) {
+
+    var answer = req.query.answer || "";
+
+    var result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
+
+		if (result){		
+			req.session.score+=1;} else {
+	req.session.score=0;
+	req.session.played=[];
+	}
+		
+		res.render('quizzes/random_result', {
+        	score: req.session.score,
+        	result: result,
+        	answer: answer
+   		 });	
+};
---
+++ models/index.js
@@ -0,0 +1,52 @@
+var path = require('path');
+
+// Cargar ORM
+var Sequelize = require('sequelize');
+
+// Para usar en local BBDD SQLite:
+//    DATABASE_URL = sqlite:///
+//    DATABASE_STORAGE = quizzes.sqlite
+// Para usar en Heroku BBDD Postgres:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
+
+var url, storage;
+
+if (!process.env.DATABASE_URL) {
+    url = "sqlite:///";
+    storage = "quizzes.sqlite";
+} else {
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
+}
+
+var sequelize = new Sequelize(url, {storage: storage});
+
+
+
+// Importar la definicion de la tabla Quiz de quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+
+// Importar la definicion de la tabla Tips de tips.js
+var Tip = sequelize.import(path.join(__dirname,'tip'));
+
+// Importar la definicion de la tabla Users de user.js
+var User = sequelize.import(path.join(__dirname,'user'));
+
+
+// Relaciones entre modelos
+Tip.belongsTo(Quiz);
+Quiz.hasMany(Tip);
+
+// Relacion 1 a N entre User y Quiz:
+User.hasMany(Quiz, {foreignKey: 'AuthorId'});
+Quiz.belongsTo(User, {as: 'Author', foreignKey: 'AuthorId'});
+
+// Relacion 1 a N entre User y Tip:
+User.hasMany(Tip, {foreignKey: 'AuthorId'} );
+Tip.belongsTo(User, {as: 'Author', foreignKey: 'AuthorId'})
+
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Tip = Tip;   // exportar definición de tabla Tips
+exports.User = User; // exportar definición de tabla Users
